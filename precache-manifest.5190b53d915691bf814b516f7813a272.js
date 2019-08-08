043180c80ae58602ed4042ff9bb2c69befd447c5
neitiris <Veliantos@gmail.com>
2019-08-08 16:09:56 +0300

    Updates

---
+++ precache-manifest.5190b53d915691bf814b516f7813a272.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d52d1db7a72a01ea1b890732f0a846",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "bbd14f0ee0f82b7abcea",
+    "url": "/react_tabs-with-router/static/css/main.203a2b44.chunk.css"
+  },
+  {
+    "revision": "76706dc3523318d70ed9",
+    "url": "/react_tabs-with-router/static/js/2.cc87f309.chunk.js"
+  },
+  {
+    "revision": "bbd14f0ee0f82b7abcea",
+    "url": "/react_tabs-with-router/static/js/main.e64a63f7.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
